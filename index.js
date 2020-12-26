b97ff5fd72e5bbdd01ac0afd012e0bc8b487d17d
Joao Pedro <leopoldino26@gmail.com>
2020-12-26 18:24:52 -0300

    Criando API e configurações iniciais

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Olá Express');
+});
+
+app.get('/nome', (req, res) => {
+    res.send('João Pedro');
+});
+
+app.listen(4000);
