ba5dd67388c5e8f0288b57ebbab16083355b6aae
Joao Pedro <leopoldino26@gmail.com>
2021-01-08 18:42:04 -0300

    Criando o CRUD Inicial

--- index.js
+++ index.js
@@ -1,12 +1,57 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
+const { Client } = require('./models');
 
-app.get('/', (req, res) => {
-    res.send('Olá Express');
+//CRUD - Create, Read, Update, Delete
+
+app.use(bodyParser.json());
+
+app.get('/', async (req, res)=>{
+    res.send("Projeto em execução...");
 });
 
-app.get('/nome', (req, res) => {
-    res.send('João Pedro');
+app.post('/clients', async (req, res)=>{
+    const client = await Client.create(req.body);
+    res.json(client);
+});
+
+app.get('/clients', async (req, res)=>{
+    const clients = await Client.findAll();
+    res.json(clients);
+});
+
+app.delete('/clients/:id', async (req, res)=>{
+    const status = await Client.destroy(
+        {
+            where:{
+                id: req.params.id
+            }
+        }
+    );
+    res.json(status);
+});
+
+app.put('/clients/:id', async (req, res)=>{
+    const client = await Client.update(req.body,
+        {
+            where:{
+                id: req.params.id
+            }
+        }
+    );
+    res.json(client);
+});
+
+app.get('/clients/:id', async (req, res)=>{
+    const client = await Client.findAll(
+        {
+            where:{
+                id: req.params.id
+            }
+        }
+    );
+    res.json(...client);
 });
 
 app.listen(4000);
